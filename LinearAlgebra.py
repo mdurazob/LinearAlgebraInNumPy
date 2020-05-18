84c58e4c5615224c9a19e3bb081f2d02310ee8bd
mdurazob <mdurazob@gmail.com>
2020-05-17 19:08:26 -0500

    Se insertó Array2Latex para tener en el clipboard la fórmula que posteriormente sería insertada en un Rmd

--- LinearAlgebra.py
+++ LinearAlgebra.py
@@ -1,31 +1,36 @@
-import numpy
-x = numpy.array([[1, 2], [4, 5]])
-y = numpy.array([[2, 6], [4, 4]])
+import numpy as np
+import array_to_latex as a2l
+
+x = np.array([[1, 2], [4, 5]])
+y = np.array([[2, 6], [4, 4]])
+
+## Copia la array a clipboard
+a2l.to_clp(x, frmt = '{:6.2f}', arraytype = 'array')
 
 print(x)
 print(y)
-print(numpy.add(x,y))
+print(np.add(x, y))
 #  add()is used to add matrices
-print ("Addition of two matrices: ")
-print (numpy.add(x,y))
+print("Addition of two matrices: ")
+print(np.add(x, y))
 # subtract()is used to subtract matrices
-print ("Subtraction of two matrices : ")
-print (numpy.subtract(x,y))
+print("Subtraction of two matrices : ")
+print(np.subtract(x, y))
 # divide()is used to divide matrices
-print ("Matrix Division : ")
-print (numpy.divide(x,y))
-print ("Multiplication of two matrices: ")
-print (numpy.multiply(x,y))
-print ("The product of two matrices : ")
-print (numpy.dot(x,y))
-print ("square root is : ")
-print (numpy.sqrt(x))
-print ("The summation of elements : ")
-print (numpy.sum(y))
-print ("The column wise summation  : ")
-print (numpy.sum(y,axis=0))
-print ("The row wise summation: ")
-print (numpy.sum(y,axis=1))
+print("Matrix Division : ")
+print(np.divide(x, y))
+print("Multiplication of two matrices: ")
+print(np.multiply(x, y))
+print("The product of two matrices : ")
+print(np.dot(x, y))
+print("square root is : ")
+print(np.sqrt(x))
+print("The summation of elements : ")
+print(np.sum(y))
+print("The column wise summation  : ")
+print(np.sum(y, axis=0))
+print("The row wise summation: ")
+print(np.sum(y, axis=1))
 # using "T" to transpose the matrix
-print ("Matrix transposition : ")
-print (x.T)
+print("Matrix transposition : ")
+print(x.T)
